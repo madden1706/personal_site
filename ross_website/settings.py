130cc8b7eaca2f265658310af603e03b81b0b40b
madden1706 <madden1706@gmail.com>
2018-08-15 13:37:19 +0100

    Changed settings.py and final sort of url slugify

--- ross_website/settings.py
+++ ross_website/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # for git
-SECRET_KEY = "wlkv5(r&n4%-08pg-(f-$0w+-rk-*7#g#i0q4jmgkh_mqh=0tt"
+# SECRET_KEY = "wlkv5(r&n4%-08pg-(f-$0w+-rk-*7#g#i0q4jmgkh_mqh=0tt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-#ALLOWED_HOSTS = ["MRD-ClassicLB-609090769.eu-west-2.elb.amazonaws.com", "MRD-env.c6fm7empee.eu-west-2.elasticbeanstalk.com", "18.130.187.160", ".madresearchden.com"]
+#ALLOWED_HOSTS = ["MRD-ClassLoadBal-1135014672.eu-west-2.elb.amazonaws.com", "MRD-env.c6fm7empee.eu-west-2.elasticbeanstalk.com ", "35.176.33.68", ".madresearchden.com"]
 ALLOWED_HOSTS = ["*"]
 
 # Application definition
