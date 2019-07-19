efac507929f129fe542acba2d59d7be6bc96f7dc
madden1706@gmail.com <madden1706@gmail.com>
2019-07-19 20:05:20 +0100

    Added an archive post view to data_vis

--- data_vis/urls.py
+++ data_vis/urls.py
@@ -1,18 +1,25 @@
 from django.urls import path, re_path
 from django.conf.urls.static import static
 from django.conf import settings
-# from django.conf.urls.static import static # To deploy. 
-from . import views
-from .views import DataVisHomepage, DataVisPost
+from django.views.generic.dates import ArchiveIndexView 
+from .views import *
+from .models import DataVis as DVModel
+
 
 
 app_name = "data_vis"
 
 urlpatterns = [
-    path('', views.DataVisHomepage.as_view(), name='data_vis'),
-    path('<slug:slug>_<pk>/', views.DataVisPost.as_view(), name="data_vis_post"),
+    path('', DataVisHomepage.as_view(), name='data_vis'),
+    path('<slug:slug>_<pk>/', DataVisPost.as_view(), name="data_vis_post"),
+    path('archive/',
+         ArchiveIndexView.as_view(model=DVModel, date_field="date_of_post"),
+         name="data_vis_archive"),
    # path('', views.testpage, name='data_vis')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# alsa_output.pci-0000_1e_00.3.analog-stereo
+
 
 
 
