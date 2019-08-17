dd1d442aab8ef5428215a88a31d6c4f28863ceb5
madden1706@gmail.com <madden1706@gmail.com>
2019-08-17 12:28:13 +0100

    Updated tests for the bokeh and altair data vis page templates.

--- data_vis/tests.py
+++ data_vis/tests.py
@@ -6,7 +6,7 @@ import datetime
 
 # Create your tests here.
 
-def create_datavis(title, days, publishable, homepage_img):
+def create_datavis(title, days, publishable, homepage_img, template):
     """ Creates a test DataVis post x days in the future and bool publishable. """
     time = timezone.now() + datetime.timedelta(days=days)
 
@@ -15,30 +15,52 @@ def create_datavis(title, days, publishable, homepage_img):
     publish=publishable,
     homepage_chart_image=homepage_img,
     intro_text = 'Intro text', 
-    seo_description = 'SEO' )
+    seo_description = 'SEO',
+    template_to_use=template)
 
 
 # Functions for making published/not posts. 
-def true_publish_post(): 
-    return create_datavis(title='Test_True', days=-1, publishable=True, 
+def true_publish_post_bokeh(): 
+    return create_datavis(title='Test_True', days=-1, publishable=True, template='bokeh',
     homepage_img='static/images/data_vis/visualization1.png')
 
-def false_publish_post(): 
-    return create_datavis(title='Test_True', days=-1, publishable=False, 
+def false_publish_post_bokeh(): 
+    return create_datavis(title='Test_True', days=-1, publishable=False, template='bokeh',
+    homepage_img='static/images/data_vis/visualization1.png')
+
+def true_publish_post_altair(): 
+    return create_datavis(title='Test_True', days=-1, publishable=True, template='altair',
+    homepage_img='static/images/data_vis/visualization1.png')
+
+def false_publish_post_altair(): 
+    return create_datavis(title='Test_True', days=-1, publishable=False, template='altair',
     homepage_img='static/images/data_vis/visualization1.png')
 
 
 class DataVisTest(TestCase):
 
-    def test_publishable_post(self):
-        test_post = true_publish_post()
+    def test_publishable_post_altair(self):
+        test_post = true_publish_post_altair()
         url = reverse('data_vis:data_vis_post', kwargs={'pk': test_post.id, 'slug': test_post.slug })
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, test_post.title_of_post)   
 
-    def test_not_publishable_post(self):
-        test_post = false_publish_post()
+    def test_not_publishable_post_altair(self):
+        test_post = false_publish_post_altair()
+        url = reverse('data_vis:data_vis_post', kwargs={'pk': test_post.id, 'slug': test_post.slug })
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_publishable_post_bokeh(self):
+        test_post = true_publish_post_bokeh()
+        url = reverse('data_vis:data_vis_post', kwargs={'pk': test_post.id, 'slug': test_post.slug })
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, test_post.title_of_post)   
+
+    def test_not_publishable_post_bokeh(self):
+        test_post = false_publish_post_bokeh()
         url = reverse('data_vis:data_vis_post', kwargs={'pk': test_post.id, 'slug': test_post.slug })
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
@@ -52,23 +74,39 @@ class DataVisHomeView(TestCase):
         self.assertContains(response, "There is no data right now...")
         self.assertQuerysetEqual(response.context['data_vis_list'], [])
 
-    def test_publishable_posts(self):
-        test_post = true_publish_post()
+    def test_publishable_posts_altair(self):
+        test_post = true_publish_post_altair()
         response = self.client.get(reverse('data_vis:data_vis'))
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(response.context['data_vis_list'], ['<DataVis: Test_True>'])
         self.assertContains(response, test_post.title_of_post)
 
-    def test_no_publishable_posts(self):
-        test_post = false_publish_post()
+    def test_no_publishable_posts_altair(self):
+        test_post = false_publish_post_altair()
         response = self.client.get(reverse('data_vis:data_vis'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "There is no data right now...")
         self.assertQuerysetEqual(response.context['data_vis_list'], [])
 
+    def test_publishable_posts_bokeh(self):
+        test_post = true_publish_post_bokeh()
+        response = self.client.get(reverse('data_vis:data_vis'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['data_vis_list'], ['<DataVis: Test_True>'])
+        self.assertContains(response, test_post.title_of_post)
+
+    def test_no_publishable_posts_bokeh(self):
+        test_post = false_publish_post_bokeh()
+        response = self.client.get(reverse('data_vis:data_vis'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There is no data right now...")
+        self.assertQuerysetEqual(response.context['data_vis_list'], [])
+
+    Only did the one combo of altair templates for this test.
     def test_publishable_and_no_publishable_posts(self):
-        test_post = true_publish_post()
-        test_post_false = false_publish_post()
+        """Only did the one combo of altair templates for this test."""
+        test_post = true_publish_post_altair()
+        test_post_false = false_publish_post_altair()
         response = self.client.get(reverse('data_vis:data_vis'))
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(response.context['data_vis_list'], ['<DataVis: Test_True>'])
