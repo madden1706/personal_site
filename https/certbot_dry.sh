72f70d1c2e883addd09a0652fe2aa3a826e66640
madden1706@gmail.com <madden1706@gmail.com>
2019-10-05 19:47:09 +0100

    Test

--- https/certbot_dry.sh
+++ https/certbot_dry.sh
@@ -1,8 +1,8 @@
 sudo docker run -it --rm \
--v /docker-volumes/etc/letsencrypt:/etc/letsencrypt \
--v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
--v /docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site:/data/letsencrypt \
--v "/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" \
+-v ../docker-volumes/etc/letsencrypt:/etc/letsencrypt \
+-v ../docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
+-v ../docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site:/data/letsencrypt \
+-v "../docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" \
 certbot/certbot \
 certonly --webroot \
 --register-unsafely-without-email --agree-tos \
