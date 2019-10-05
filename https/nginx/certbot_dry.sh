b10f6d3cec3a71fe876bed0bf3a9ce2727f8b35f
madden1706@gmail.com <madden1706@gmail.com>
2019-10-05 18:31:50 +0100

    Updated format.

--- https/nginx/certbot_dry.sh
+++ https/nginx/certbot_dry.sh
@@ -1,7 +1,7 @@
-sudo docker run -it --rm \
--v /docker-volumes/etc/letsencrypt:                                         /etc/letsencrypt \
--v /docker-volumes/var/lib/letsencrypt:                                     /var/lib/letsencrypt \
--v /docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site:       /data/letsencrypt \
+sudo docker run -it --rm letsencrypt_nginx\
+-v /docker-volumes/etc/letsencrypt:/etc/letsencrypt \
+-v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
+-v /docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site:/data/letsencrypt \
 -v "/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" \
 certbot/certbot \
 certonly --webroot \
