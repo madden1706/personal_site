ccdd03fd8703c74003a6928344b683d3398c3799
madden1706@gmail.com <madden1706@gmail.com>
2019-11-03 19:51:25 +0000

    Added logic to feature data vis interative posts on the main page over a normal data vis post. Also reformats the blogpost arrangemnt.

--- website/homepage/tests.py
+++ website/homepage/tests.py
@@ -1,3 +1,68 @@
+
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+from data_vis.models import DataVis,DataVisInteractive
 
 # Create your tests here.
+
+
+def create_data_vis_int(title, date, post, publishable):
+    """
+    :param title: Title of a blog post.
+    :param date: Date of the blog post.
+    :param post: Content of a blog post.
+    :return: An interactive data vis post with a specified date, +int for future (days) and -int for past.
+    """
+
+    time = timezone.now() + datetime.timedelta(days=date)
+
+    return DataVisInteractive.objects.create(title_of_post=title, 
+        date_of_post=time, 
+        intro_text=post, 
+        publish=publishable)
+
+
+def create_data_vis(title, date, post, publishable):
+    """
+    :param title: Title of a blog post.
+    :param date: Date of the blog post.
+    :param post: Content of a blog post.
+    :return: A data vis post with a specified date, +int for future (days) and -int for past.
+    """
+
+    time = timezone.now() - datetime.timedelta(days=date)
+
+    return DataVis.objects.create(title_of_post=title, 
+        date_of_post=time, 
+        intro_text=post, 
+        publish=publishable)
+
+
+class HomepageDataVis(TestCase):
+
+    # python manage.py test homepage
+
+    def test_main_datavis_int_post(self):        
+        """
+        
+        :return:
+        """
+        test = create_data_vis_int("Data Vis Int Title", -10, "Post text", True)
+        response = self.client.get(reverse('homepage:homepage'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, test.title_of_post)
+        #self.assertQuerysetEqual(response.context['data_vis_int_main'], f'<DataVisInteractive: {test.title_of_post}>')
+
+
+    # def test_main_datavis_post(self):
+    #     """
+        
+    #     :return:
+    #     """
+
+    #     create_data_vis("Data Vis Title", 10, "Post text", True)
+    #     response = self.client.get(reverse('homepage:homepage'))
+    #     self.assertContains(response, "Data Vis Title")
+    #     #self.assertQuerysetEqual(response.context['data_vis_main'], [])
--- website/homepage/views.py
+++ website/homepage/views.py
@@ -1,64 +1,102 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView,DetailView
+from django.utils import timezone
 
 from blog.models import BlogPost
-from data_vis.models import DataVis
+from data_vis.models import DataVis, DataVisInteractive
 
-from django.utils import timezone
+import datetime
 
 # Create your views here.
 
 
+
 def homepage(self):
 
-    # If there are no posts the logic in the homeplate html template displays a message.
-    try:
-        blogpost = BlogPost.objects.filter(
-            date_of_post__lte=timezone.now()
-            ).filter(publish=True
-            ).order_by("-date_of_post")[0]               
-    except:
-        blogpost = []
-    
-    try:
-        secondary_posts = BlogPost.objects.filter(
-            date_of_post__lte=timezone.now()
-            ).filter(publish=True
-            ).order_by("-date_of_post")[1:3]               
-    except:
-        secondary_posts = []
-        
+    """
+    If there is an interactive post return that.
+    If not return a data vis post.
+
+    Return most recent 3 blog posts.
+    """
+
+    data_vis_main = []
+    data_vis_int_main = []
+    blogpost = []
+    secondary_posts = []
+    secondary_data_vis = []
 
     try:
-        data_vis = DataVis.objects.filter(
+        data_vis_main = DataVis.objects.filter(
             date_of_post__lte=timezone.now()
             ).filter(
             publish=True
-            ).order_by("-date_of_post")[1]
+            ).order_by("-date_of_post")[0]
     except:
-        data_vis = []
+        data_vis_main = []
 
     try:
-        secondary_data_vis = DataVis.objects.filter(
+        data_vis_int_main = DataVisInteractive.objects.filter(
+            date_of_post__lte=timezone.now()
+            ).filter(
+            publish=True
+            ).order_by("-date_of_post")[0]
+    except:
+        data_vis_int_main = []
+
+    if data_vis_main and data_vis_int_main:
+        
+        # Feature an interactive post over a normal most recent data vis post. 30 day limit. 
+        if (data_vis_main.date_of_post - datetime.timedelta(days=30)) > data_vis_int_main.date_of_post:
+            data_vis_int_main = []
+        elif (data_vis_main.date_of_post - datetime.timedelta(days=30)) < data_vis_int_main.date_of_post:
+            data_vis_main = []
+        else:
+            data_vis_main = []
+
+
+    elif data_vis_main and not data_vis_int_main:
+        data_vis_int_main = []
+    
+    elif not data_vis_main and data_vis_int_main:
+        data_vis_main = []
+        
+
+    else:
+        data_vis_main = []
+        data_vis_int_main = []
+
+    # Try to get the most recent blogpost
+    try:
+        blogpost = BlogPost.objects.filter(
+            date_of_post__lte=timezone.now()
+            ).filter(
+            publish=True
+            ).order_by("-date_of_post")[0]
+    except:
+        blogpost = []
+
+    # If there is a main blogpost - get older posts as secondary posts to feature. 
+    if blogpost:
+        secondary_blogposts = BlogPost.objects.filter(
             date_of_post__lte=timezone.now()
             ).filter(
             publish=True
             ).order_by("-date_of_post")[1:3]
-    except:
-        secondary_data_vis = []
+    else:
+        secondary_blogposts = []    
 
-    data = {'datavis': data_vis, 
+
+    data = {'data_vis_main': data_vis_main,
+        'data_vis_int_main': data_vis_int_main, 
         'blogpost': blogpost, 
-        'secondary_blogposts': secondary_posts,
+        'secondary_blogposts': secondary_blogposts,
         'secondary_datavis': secondary_data_vis}
 
     template = 'homepage/homepage.html'
 
     return render(self, template, data)
-
-
-
 
 
 # def homepage(self): 
