bb30188d4adfc7e6cce00ff5474e9d42cd4dd4d7
madden1706@gmail.com <madden1706@gmail.com>
2020-12-23 17:27:57 +0000

    Added logging to debug server error.

--- website/madresearchden/madresearchden/settings/production.py
+++ website/madresearchden/madresearchden/settings/production.py
@@ -1,8 +1,32 @@
 from .base import *
+import os
 
 DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*']#os.environ['ALLOWED_HOSTS'].split('|')
 
 try:
     from .local import *
 except ImportError:
     pass
+
+import logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
